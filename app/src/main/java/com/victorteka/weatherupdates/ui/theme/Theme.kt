23a2275f3075b143a6942c74e9c1eb8d2fcefadf
Victor Teka <tekav@letshego.com>
2023-09-01 15:42:39 +0300

    Set up theme

--- app/src/main/java/com/victorteka/weatherupdates/ui/theme/Theme.kt
+++ app/src/main/java/com/victorteka/weatherupdates/ui/theme/Theme.kt
@@ -10,31 +10,23 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val LightColorPalette = lightColorScheme(
+    primary = pink500,
+    secondary = pink500,
+    onPrimary = Color.Black,
+    onSecondary = Color.Black
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val DarkColorPalette = darkColorScheme(
+    primary = pink200,
+    secondary = pink200,
+    surface = pinkDarkPrimary
 )
 
 @Composable
@@ -50,8 +42,8 @@ fun WeatherUpdatesTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> DarkColorPalette
+        else -> LightColorPalette
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
@@ -64,7 +56,7 @@ fun WeatherUpdatesTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = typography,
         content = content
     )
 }
--- app/src/main/java/com/victorteka/weatherupdates/ui/theme/Type.kt
+++ app/src/main/java/com/victorteka/weatherupdates/ui/theme/Type.kt
@@ -2,33 +2,122 @@ package com.victorteka.weatherupdates.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.victorteka.weatherupdates.R
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+
+private val fonts = FontFamily(
+    Font(R.font.rubik_regular),
+    Font(R.font.rubik_medium, FontWeight.W500),
+    Font(R.font.rubik_bold, FontWeight.Bold)
+)
+
+val typography = typographyFromDefaults(
+    displayLarge = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold
+    ),
+    displayMedium = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold
+    ),
+    displaySmall = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        lineHeight = 40.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.W500,
+        lineHeight = 28.sp
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
+        fontWeight = FontWeight.W500,
+        lineHeight = 22.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.W500
+    ),
+    titleSmall = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.W500
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        lineHeight = 28.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 16.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 16.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold
+    ),
+    labelMedium = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold
     )
-    */
 )
+
+fun typographyFromDefaults(
+    displayLarge: TextStyle?,
+    displayMedium: TextStyle?,
+    displaySmall: TextStyle?,
+    headlineLarge: TextStyle?,
+    headlineMedium: TextStyle?,
+    headlineSmall: TextStyle?,
+    titleLarge: TextStyle?,
+    titleMedium: TextStyle?,
+    titleSmall: TextStyle?,
+    bodyLarge: TextStyle?,
+    bodyMedium: TextStyle?,
+    bodySmall: TextStyle?,
+    labelLarge: TextStyle?,
+    labelMedium: TextStyle?,
+    labelSmall: TextStyle?,
+): Typography {
+    val defaults = Typography()
+    return Typography(
+        displayLarge = defaults.displayLarge.merge(displayLarge),
+        displayMedium = defaults.displayMedium.merge(displayMedium),
+        displaySmall = defaults.displaySmall.merge(displaySmall),
+        headlineLarge = defaults.headlineLarge.merge(headlineLarge),
+        headlineMedium = defaults.headlineMedium.merge(headlineMedium),
+        headlineSmall = defaults.headlineSmall.merge(headlineSmall),
+        titleLarge = defaults.titleLarge.merge(titleLarge),
+        titleMedium = defaults.titleMedium.merge(titleMedium),
+        titleSmall = defaults.titleSmall.merge(titleSmall),
+        bodyLarge = defaults.bodyLarge.merge(bodyLarge),
+        bodyMedium = defaults.bodyMedium.merge(bodyMedium),
+        bodySmall = defaults.bodySmall.merge(bodySmall),
+        labelLarge = defaults.labelLarge.merge(labelLarge),
+        labelMedium = defaults.labelMedium.merge(labelMedium),
+        labelSmall = defaults.labelSmall.merge(labelSmall)
+    )
+}
