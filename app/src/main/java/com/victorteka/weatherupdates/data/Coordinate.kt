ce91b8dedffde1730e00fca08bed6b1f04aa6bfc
Victor Teka <tekav@letshego.com>
2023-09-04 16:52:25 +0300

    change cities coordinates

--- app/src/main/java/com/victorteka/weatherupdates/data/Coordinate.kt
+++ app/src/main/java/com/victorteka/weatherupdates/data/Coordinate.kt
@@ -3,14 +3,15 @@ package com.victorteka.weatherupdates.data
 data class Coordinate(val latitude: Double, val longitude: Double)
 
 val coordinates = listOf(
-    Coordinate(latitude = 53.619653, longitude = 10.079969),
-    Coordinate(latitude = 53.080917, longitude = 8.847533),
-    Coordinate(latitude = 52.378385, longitude = 9.794862),
-    Coordinate(latitude = 52.496385, longitude = 13.444041),
-    Coordinate(latitude = 53.866865, longitude = 10.739542),
-    Coordinate(latitude = 54.304540, longitude = 10.152741),
-    Coordinate(latitude = 54.797277, longitude = 9.491039),
-    Coordinate(latitude = 52.426412, longitude = 10.821392),
-    Coordinate(latitude = 53.542788, longitude = 8.613462),
-    Coordinate(latitude = 53.141598, longitude = 8.242565)
+    Coordinate(latitude = -1.286389, longitude = 36.817223),
+    Coordinate(latitude = -4.043740, longitude = 39.658871),
+    Coordinate(latitude = -0.091702, 34.767956),
+//    Coordinate(latitude = -0.303099, longitude = 36.080025),
+    Coordinate(latitude = -3.219186, longitude = 40.116890),
+    Coordinate(latitude = -0.680482, longitude = 4.777061),
+    Coordinate(latitude = -1.375081, longitude = 37.995213),
+    Coordinate(latitude = 0.569525, longitude = 34.558376),
+    Coordinate(latitude = -0.717178, longitude = 36.431026),
+    Coordinate(latitude = 0.514277, longitude = 35.269779),
+    Coordinate(latitude = 1.019089, longitude = 35.002304)
 )
