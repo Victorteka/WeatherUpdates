ce3f90f27e09666a51a473563040c5c054efbc36
Victor Teka <tekav@letshego.com>
2023-09-04 18:21:52 +0300

    change cities coordinates

--- app/src/main/java/com/victorteka/weatherupdates/data/Coordinate.kt
+++ app/src/main/java/com/victorteka/weatherupdates/data/Coordinate.kt
@@ -6,12 +6,7 @@ val coordinates = listOf(
     Coordinate(latitude = -1.286389, longitude = 36.817223),
     Coordinate(latitude = -4.043740, longitude = 39.658871),
     Coordinate(latitude = -0.091702, 34.767956),
-//    Coordinate(latitude = -0.303099, longitude = 36.080025),
-    Coordinate(latitude = -3.219186, longitude = 40.116890),
-    Coordinate(latitude = -0.680482, longitude = 4.777061),
     Coordinate(latitude = -1.375081, longitude = 37.995213),
-    Coordinate(latitude = 0.569525, longitude = 34.558376),
-    Coordinate(latitude = -0.717178, longitude = 36.431026),
     Coordinate(latitude = 0.514277, longitude = 35.269779),
     Coordinate(latitude = 1.019089, longitude = 35.002304)
 )
